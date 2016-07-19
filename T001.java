39fd4aa6faab50873d07c9fd2bbf6f83b0f7f094
blyang <clear_d@163.com>
2016-07-19 11:36:11 +0800

    modified T001

--- T001.java
+++ T001.java
@@ -2,21 +2,36 @@ package com.blyang;
 
 public class T001 {
 
-    public int[] twoSum(int[] nums, int target) {
-        int[] result = new int[2];
+    // public int[] twoSum(int[] nums, int target) {
+    //     int[] result = new int[2];
         
-        int length = nums.length;
-        for(int i=0; i<length-1; i++){
-            for(int j=i+1; j<length; j++){
-                if(target == (nums[i] + nums[j])){
-                    result[0] = i;
-                    result[1] = j;
-                    return result;
-                }
-            }
-        }
+    //     int length = nums.length;
+    //     for(int i=0; i<length-1; i++){
+    //         for(int j=i+1; j<length; j++){
+    //             if(target == (nums[i] + nums[j])){
+    //                 result[0] = i;
+    //                 result[1] = j;
+    //                 return result;
+    //             }
+    //         }
+    //     }
         
-        return result;
+    //     return result;
+    // }
+
+
+    public int[] twoSum(int[] nums, int target){
+    	
+    	Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+    	
+    	for(int i=0; i<nums.length; i++){
+    		if(map.containsKey(target-nums[i])){
+    			return new int[]{map.get(target-nums[i]), i};
+    		}
+    		map.put(nums[i], i);
+    	}
+    	
+    	return null;
     }
 
 	public static void main(String[] args) {
